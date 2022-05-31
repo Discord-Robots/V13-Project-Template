ac654822e00b7fa5b17ec0c17daac5d3a2d2d5c6
George Needham <georgeneedham7@gmail.com>
2022-05-31 02:22:49 -0700

    add events

---
+++ Events/Guild/guildCreate.js
@@ -0,0 +1,13 @@
+const { Client, Guild } = require("discord.js");
+
+module.exports = {
+    name: "guildCreate",
+    /**
+     * 
+     * @param {Client} client 
+     * @param {Guild} guild 
+     */
+    async execute(client, guild) {
+        
+    }
+}
---
+++ Events/Guild/guildDelete.js
@@ -0,0 +1,13 @@
+const { Client, Guild } = require("discord.js");
+
+module.exports = {
+    name: "guildDelete",
+    /**
+     * 
+     * @param {Client} client 
+     * @param {Guild} guild 
+     */
+    async execute(client, guild) {
+        
+    }
+}
