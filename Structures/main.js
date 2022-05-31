bd272301eb9d733b5f6ab479a3411137db44ad02
George Needham <georgeneedham7@gmail.com>
2022-05-31 10:30:30 -0500

    update main.js

--- Structures/main.js
+++ Structures/main.js
@@ -1,14 +1,14 @@
 require("dotenv").config();
 const { Client, Collection } = require("discord.js");
 const client = new Client({
-  intents: 32767,
+  intents: 131071,
   partials: [
     "CHANNEL",
-    "USER",
     "MESSAGE",
-    "REACTION",
     "GUILD_MEMBER",
     "GUILD_SCHEDULED_EVENT",
+    "USER",
+    "REACTION",
   ],
   presence: {
     status: "dnd",
@@ -16,19 +16,21 @@ const client = new Client({
     afk: true,
   },
 });
-const Utilities = require("./Utils");
-const guildData = {};
-
 const { promisify } = require("util");
 const Ascii = require("ascii-table");
 const { glob } = require("glob");
 const PG = promisify(glob);
-
-client.commands = new Collection();
-client.filters = new Collection();
-client.filtersLog = new Collection();
-client.functions = new Utilities();
-const funcs = client.functions;
+const Util = require("./Utils");
+const guildData = {};
+let funcs;
+(async () => {
+  client.commands = new Collection();
+  client.filters = new Collection();
+  client.filtersLog = new Collection();
+  client.functions = new Util();
+  client.login(process.env.TOKEN);
+  funcs = client.functions;
+})();
 
 ["GiveawaySys"].forEach((system) => {
   require(`../Systems/${system}`)(client);
@@ -37,5 +39,3 @@ const funcs = client.functions;
 ["Events", "Commands"].forEach((handler) => {
   require(`./Handlers/${handler}`)(client, PG, Ascii, guildData, funcs);
 });
-
-client.login(process.env.TOKEN);
