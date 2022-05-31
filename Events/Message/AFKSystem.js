c1ed2ab5ac9e9d63f54d06d051a3c28ec10d1422
needhamgary <garyneedham1@gmail.com>
2022-05-31 12:24:19 -0400

    afk

--- Events/Message/AFKSystem.js
+++ Events/Message/AFKSystem.js
@@ -1,27 +1,35 @@
-const { Message, MessageEmbed } = require("discord.js");
+const { Message, MessageEmbed, Client } = require("discord.js");
 const DB = require("../../Structures/Schemas/AFKSystem");
 
 module.exports = {
-    name: "messageCreate",
-    /**
-     * 
-     * @param {Message} message 
-     */
-    async execute(message) {
-        if(message.author.bot) return;
+  name: "messageCreate",
+  /**
+   *
+   * @param {Message} message
+   * @param {Client} client
+   */
+  async execute(message, client) {
+    if (message.author.bot) return;
 
-        // await DB.deleteOne({GuildID: message.guild.id, UserID: message.author.id});
-
-        if(message.mentions.members.size) {
-            const Embed = new MessageEmbed()
-            .setColor("RED");
-            message.mentions.members.forEach((m) => {
-                DB.findOne({GuildID: message.guild.id, UserID: m.id}, async (err, data) => {
-                    if(err) throw err;
-                    if(data) 
-                    return message.reply({embeds: [Embed.setDescription(`${m} went AFK <t:${data.Time}:R>\n **Status**: ${data.Status}`)]});
-                } )
-            })
-        }
+    // await DB.deleteOne({GuildID: message.guild.id, UserID: message.author.id});
+    let mentioned = message.mentions.members;
+    if (mentioned.size) {
+      const Embed = new MessageEmbed()
+        .setColor("RED")
+        .setFooter({ text: `${client.user.username}'s AFK System` });
+      mentioned.forEach((m) => {
+        DB.findOne({ GuildID: m.guild.id, UserID: m.id }, async (err, data) => {
+          if (err) throw err;
+          if (data)
+            return message.reply({
+              embeds: [
+                Embed.setDescription(
+                  `${m} went AFK <t:${data.Time}:R>\n **Status**: ${data.Status}`
+                ),
+              ],
+            });
+        });
+      });
     }
-}
+  },
+};
