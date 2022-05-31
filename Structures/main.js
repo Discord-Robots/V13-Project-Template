2df21496ec8dbe3dd0c55f963571c56ce8c69bda
needhamgary <garyneedham1@gmail.com>
2022-05-31 06:27:46 -0400

    undo merge

--- Structures/main.js
+++ Structures/main.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
-const { Client, Collection } = require("discord.js");
-const client = new Client({ intents: 32767 });
+const { Client, Collection, Util } = require("discord.js");
+const client = new Client({ intents: 131071 });
+const Util = require("./Utils");
+const guildData = {};
 
 const { promisify } = require("util");
 const Ascii = require("ascii-table");
@@ -10,13 +12,15 @@ const PG = promisify(glob);
 client.commands = new Collection();
 client.filters = new Collection();
 client.filtersLog = new Collection();
+client.functions = new Util();
+const funcs = client.functions;
 
 ["GiveawaySys"].forEach((system) => {
   require(`../Systems/${system}`)(client);
 });
 
 ["Events", "Commands"].forEach((handler) => {
-  require(`./Handlers/${handler}`)(client, PG, Ascii);
+  require(`./Handlers/${handler}`)(client, PG, Ascii, guildData, funcs);
 });
 
 client.login(process.env.TOKEN);
