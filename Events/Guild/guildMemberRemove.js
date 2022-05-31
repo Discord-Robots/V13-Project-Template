dd4157a45df588fe8f53440f2ea08977e3352771
George Needham <georgeneedham7@gmail.com>
2022-05-31 10:27:37 -0500

    update gmr

--- Events/Guild/guildMemberRemove.js
+++ Events/Guild/guildMemberRemove.js
@@ -1,19 +1,12 @@
 const { Client, GuildMember } = require("discord.js");
 
 module.exports = {
-    name: "guildMemberRemove",
-    /**
-     * @param {Client} client
-     * @param {GuildMember} member
-     */
-    async execute(client, member) {
-        console.log(member.user.username + "has left the server.")
-        let guild = client.guilds.cache.get(member.guild.id)
-        // Use either channel to send the message, comment out the one you don't use
-        let channel = guild.channels.cache.find(ch => ch.name == "")
-        let channel = guild.channels.cache.get()
-        channel.send(member.user.tag + "left the server")
-
-    }
-}
-
+  name: "guildMemberRemove",
+  /**
+   * @param {Client} client
+   * @param {GuildMember} member
+   */
+  async execute(client, member) {
+    console.log(member.user.username + "has left the server.");
+  },
+};
